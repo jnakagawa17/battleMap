7489bdec392f8bdb6040e0f238a2ff28c7bb0d20
jnakagawa17 <jnakagawa17@punahou.edu>
2014-12-15 15:32:35 -1000

    Mechanics
    I fixed the declaration of different classes and made code to draw them.

--- MeggyJr_Blink/MeggyJr_Blink.ino
+++ MeggyJr_Blink/MeggyJr_Blink.ino
@@ -39,33 +39,34 @@
 
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
 
+int choice[6] = {1, 2, 3, 1, 2, 3};
+
 struct Point
 {
   int x; // coordinates of a dot
   int y;
 };
 
-struct Heavy
+struct Class
 {
   int health;
   int defense;
   int attack;
   int availability; 
   Point p1;
-  Point p2;
-  Point p3;
-  Point p4;
 };
 
-Heavy t1 = {8, 6, 3, 1, {0, 7}, {0, 6}, {1, 7}, {1, 6}};
-Heavy t2 = {8, 6, 3, 1, {0, 3}, {0, 4}, {1, 3}, {1, 4}};
-Heavy t3 = {8, 6, 3, 1, {0, 0}, {0, 1}, {1, 0}, {1, 1}};
-Heavy t4 = {8, 6, 3, 1, {3, 7}, {3, 6}, {4, 7}, {4, 6}};
-Heavy t5 = {8, 6, 3, 1, {3, 3}, {3, 4}, {4, 3}, {4, 4}};
-Heavy t6 = {8, 6, 3, 1, {3, 0}, {3, 1}, {4, 0}, {4, 1}};
+Class t1 = {0, 0, 0, 0, {0, 7}};
+Class t2 = {0, 0, 0, 0, {0, 4}};
+Class t3 = {0, 0, 0, 0, {0, 1}};
+Class t4 = {0, 0, 0, 0, {3, 7}};
+Class t5 = {0, 0, 0, 0, {3, 4}};
+Class t6 = {0, 0, 0, 0, {3, 1}};
 
-Heavy tankLocations[6] = {t1, t2, t3, t4, t5, t6};
+Class Locations[6] = {t1, t2, t3, t4, t5, t6};
 
+int field[6];
+/*
 struct Light
 {
   int health;
@@ -76,15 +77,6 @@ struct Light
   Point p2;
 };
 
-Light l1 = {8, 6, 3, 1, {0, 7}, {1, 7}};
-Light l2 = {8, 6, 3, 1, {0, 4}, {1, 4}};
-Light l3 = {8, 6, 3, 1, {0, 1}, {1, 1}};
-Light l4 = {8, 6, 3, 1, {3, 7}, {4, 7}};
-Light l5 = {8, 6, 3, 1, {3, 4}, {4, 4}};
-Light l6 = {8, 6, 3, 1, {3, 1}, {4, 1}};
-
-Light scoutLocation[6] = {l1, l2, l3, l4, l5, l6};
-
 struct Special
 {
   int health;
@@ -94,18 +86,27 @@ struct Special
   Point p1;
   Point p2;
   Point p3;
-  Point p4;
 };
 
-Special t1 = {8, 6, 3, 1, {0, 7}, {0, 6}, {1, 7}, {1, 6}};
-Special t2 = {8, 6, 3, 1, {0, 3}, {0, 4}, {1, 3}, {1, 4}};
-Special t3 = {8, 6, 3, 1, {0, 0}, {0, 1}, {1, 0}, {1, 1}};
-Special t4 = {8, 6, 3, 1, {3, 7}, {3, 6}, {4, 7}, {4, 6}};
-Special t5 = {8, 6, 3, 1, {3, 3}, {3, 4}, {4, 3}, {4, 4}};
-Special t6 = {8, 6, 3, 1, {3, 0}, {3, 1}, {4, 0}, {4, 1}};
+Light l1 = {8, 6, 3, 1, {0, 7}, {1, 7}};
+Light l2 = {8, 6, 3, 1, {0, 4}, {1, 4}};
+Light l3 = {8, 6, 3, 1, {0, 1}, {1, 1}};
+Light l4 = {8, 6, 3, 1, {3, 7}, {4, 7}};
+Light l5 = {8, 6, 3, 1, {3, 4}, {4, 4}};
+Light l6 = {8, 6, 3, 1, {3, 1}, {4, 1}};
 
-Heavy tankLocations[6] = {t1, t2, t3, t4, t5, t6};
+Special s1 = {8, 6, 3, 1, {0, 7}, {0, 6}, {1, 6}};
+Special s2 = {8, 6, 3, 1, {0, 3}, {0, 4}, {1, 3}};
+Special s3 = {8, 6, 3, 1, {0, 0}, {0, 1}, {1, 0}};
+Special s4 = {8, 6, 3, 1, {3, 6}, {4, 7}, {4, 6}};
+Special s5 = {8, 6, 3, 1, {3, 3}, {4, 3}, {4, 4}};
+Special s6 = {8, 6, 3, 1, {3, 0}, {4, 0}, {4, 1}};
 
+
+Light scoutLocation[6] = {l1, l2, l3, l4, l5, l6};
+
+Special mageLocations[6] = {s1, s2, s3, s4, s5, s6};
+*/
 
 void setup()                    // run once, when the sketch starts
 {
@@ -114,8 +115,35 @@ void setup()                    // run once, when the sketch starts
 
 void loop()                     // run over and over again
 {
-  
+  updateLocations();
+  DisplaySlate();
 
 }
+
+void updateLocations()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    if (choice[i] == 1)
+    {
+      DrawPx(Locations[i].p1.x, Locations[i].p1.y, Green);
+      DrawPx(Locations[i].p1.x + 1, Locations[i].p1.y, Green);
+      DrawPx(Locations[i].p1.x, Locations[i].p1.y - 1, Green);
+      DrawPx(Locations[i].p1.x + 1, Locations[i].p1.y - 1, Green);
+    }
+    if (choice[i] == 2)
+    {
+      DrawPx(Locations[i].p1.x, Locations[i].p1.y, Green);
+      DrawPx(Locations[i].p1.x + 1, Locations[i].p1.y, Green);
+    }    
+    if (choice[i] == 3)
+    {
+      DrawPx(Locations[i].p1.x, Locations[i].p1.y, Green);
+      DrawPx(Locations[i].p1.x, Locations[i].p1.y - 1, Green);
+      DrawPx(Locations[i].p1.x + 1, Locations[i].p1.y - 1, Green);
+    }  
+  }
+}
+
 
 
